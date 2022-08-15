a19ba6c90c44bef87840b2ba8149734ff2e98faf
Damian Soria <damian.soria@engworks.com>
2022-08-15 12:14:56 -0300

    fix: update method

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,31 @@ import { useState } from "react";
 import { isExist } from "./utils/validationHelper";
 
 export default function App() {
-  const [options, setOptions] = useState<IdValue[]>([]);
+  const [options, setOptions] = useState<IdValue[]>(buildingAreasOptions);
+  const [values, setValues] = useState<IdValue[]>([]);
 
   function handleChecked(item: any) {
     console.log(item);
-    if (isExist(options, item.id).length) {
-      const removeOption = options.filter((opt) => opt.id !== item.id)
-      setOptions(removeOption)
+    if (isExist(values, item.id).length) {
+      const disabledInput: IdValue[] = options.map((option: IdValue) => {
+        if (option.id === item.id) {
+          option.disabled = true;
+        }
+        return option;
+      });
+      setOptions(disabledInput);
+      const removeOption = values.filter((opt) => opt.id !== item.id);
+      setValues(removeOption);
       return;
     }
-    return setOptions([...options, item])
+    const enableInput: IdValue[] = options.map((option: IdValue) => {
+      if (option.id === item.id) {
+        option.disabled = false;
+      }
+      return option;
+    });
+    setOptions(enableInput);
+    return setValues([...values, item]);
   }
 
   return (
